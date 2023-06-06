3ecfeec8c6f81f3a98001640adb9a77cc23fc347
Matheus Link <122500941+MthsLnk-gthb@users.noreply.github.com>
2023-06-06 15:36:18 -0300

    Files add
    I added the project files in the repository, i'm trying to launch him at github pages

---
+++ Portfolio/main.js
@@ -0,0 +1,7 @@
+const github = document.getElementById('github')
+
+function githubAcess(){
+    window.location.href = 'https://github.com/MthsLnk-gthb'
+}
+
+github.addEventListener('click', githubAcess)
